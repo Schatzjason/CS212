befe45f8d48a4942c53f74fc10dcbee61540f826
Jason Schatz <jason.schatz@udacity.com>
2017-09-06 14:15:11 -0700

    modify Click Count

--- LectureCode/ClickCount/ClickCount/ViewController.swift
+++ LectureCode/ClickCount/ClickCount/ViewController.swift
@@ -9,14 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    let kCount = "click-count"
-    
-    // This is the model class, See Counter.swift
+
+    // This is the simple model class, See Counter.swift
+    // (Notice the implicitly unwrapped optional)
     var counter: Counter!
     
     // A reference to the label in the view heirarchy
     // We need this so that we can set its text
+    // (Notice the plain old optional)
     var label: UILabel?
     
     // View Did Load:
@@ -27,10 +27,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Find the app delegate, in order to get its counter
+        // Here we initialize our model object
         self.counter = Counter(initialValue: 0)
         
         // Make a label object
+        // (Notice how we need to keep forcibly unwrapping it. A pain.)
         let frame = CGRect(x: 125, y: 120, width: 100, height: 50)
         label = UILabel(frame: frame)
         label!.font = UIFont.systemFont(ofSize: 70)
@@ -49,12 +50,10 @@ class ViewController: UIViewController {
     }
     
     // This is the method that the button will call when it is tapped
-    
     func incrementTheCount() {
         counter.increment()
         label!.text = "\(counter.value)"
     }
-    
 }
 
 
