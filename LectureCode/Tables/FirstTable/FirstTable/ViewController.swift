e276cac15a278e7afc7f327058d5e89c7d5021e3
Jason Schatz <jason.schatz@udacity.com>
2017-10-19 20:03:11 -0700

    Working first table

--- LectureCode/Tables/FirstTable/FirstTable/ViewController.swift
+++ LectureCode/Tables/FirstTable/FirstTable/ViewController.swift
@@ -8,23 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    // Remember which rows have check marks
+    var selectedIndexes = Set<Int>()
+
+    // Remeber some items for our table
+    var fruits = ["Apple", "Mango", "Lemon", "Pear", "Orange", "Pineapple", "Persimon"]
+    
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5000
+        // The size of the fruits array determines the number of rows in the table
+        return fruits.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Get a cell to decorate
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell1")!
-        cell.textLabel?.text = "Hello \(indexPath.row)"
-        print("Get Cell Number \(indexPath.row)")
+        
+        // Set the text
+        cell.textLabel?.text = fruits[indexPath.row]
+        
+        // Set or unset the checkmark
+        if self.selectedIndexes.contains(indexPath.row) {
+            cell.accessoryType = UITableViewCellAccessoryType.checkmark
+        } else {
+            cell.accessoryType = UITableViewCellAccessoryType.none
+        }
+        
+        // Send the cell back to the table
         return cell
     }
     
+    // Invoked when a table cell is selected
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // Toggle whether or not the row # is in the set
+        if selectedIndexes.contains(indexPath.row) {
+            self.selectedIndexes.remove(indexPath.row)
+        } else {
+            self.selectedIndexes.insert(indexPath.row)
+        }
+                
+        // Reload the row, so that we can see the change
+        tableView.reloadRows(at: [indexPath], with: .fade)
+    }
 }
 
